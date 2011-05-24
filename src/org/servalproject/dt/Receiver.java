1fd4707464099627192721149a5fc1f282d22021
Thomas.Giraud <thomas.giraud@gmail.com>
2011-05-24 11:46:26 +0930

    Easier name of Intent for DT and Notification starts an activity to read SMS

--- src/org/servalproject/dt/Receiver.java
+++ src/org/servalproject/dt/Receiver.java
@@ -6,18 +6,22 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 
 public class Receiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(final Context context, final Intent intent) {
-		if ((intent.getAction().equals("android.intent.action.PICK"))
-				& (intent.getType()
-						.equals("vnd.servalproject.DTSMS/vnd.servalproject.DTSMS-text"))) {
+		Log.i("Receiver", "Intent received " + intent.getAction());
+		// if ((intent.getAction().equals("android.intent.action.DEFAULT"))
+		if ((intent.getAction().equals("org.servalproject.DT"))
+		/*
+		 * & (intent.getType()
+		 * .equals("vnd.servalproject.DT/vnd.servalproject.DT"))
+		 */) {
 			String sender = intent.getExtras().getString("number");
 			String content = intent.getExtras().getString("content");
 			this.writeSMS(sender, content, context);
@@ -44,16 +48,21 @@ public class Receiver extends BroadcastReceiver {
 		Notification notification = new Notification(icon, tickerText, when);
 		// Context context = getApplicationContext();
 		CharSequence contentTitle = "Digital Telegram";
-		CharSequence contentText = "You have a new message";
-		Intent notificationIntent = new Intent("android.intent.action.MAIN");
+		CharSequence contentText = sender + ": " + content;
+		// Intent notificationIntent = new Intent("android.intent.action.MAIN");
+		Intent notificationIntent = new Intent("android.intent.action.VIEW");
+		notificationIntent.setData(Uri
+				.parse("content://mms-sms/conversations/"));
 		// Lauch Messaging when the user clicks on the notification
 		// notificationIntent.setComponent(new
 		// ComponentName("com.android.mms","com.android.mms.ui.ConversationList"));
 		// Lauch SmsDroid when the user clicks on the notification
-		notificationIntent.setComponent(new ComponentName(
-				"de.ub0r.android.smsdroid",
-				"de.ub0r.android.smsdroid.ConversationList"));
-		PendingIntent contentIntent = PendingIntent.getBroadcast(context, 0,
+		/*
+		 * notificationIntent.setComponent(new ComponentName(
+		 * "de.ub0r.android.smsdroid",
+		 * "de.ub0r.android.smsdroid.ConversationList"));
+		 */
+		PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
 				notificationIntent, 0);
 
 		notification.setLatestEventInfo(context, contentTitle, contentText,
